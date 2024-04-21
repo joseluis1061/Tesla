85351e0d302bacd9ec2b98727a5828df18430cbe
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-21 12:27:07 -0500

    Inicio del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../app/modules/webside/webside.module').then(m => m.WebsideModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/webside/webside.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WebsideRoutingModule } from './webside-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WebsideRoutingModule
+  ]
+})
+export class WebsideModule { }
