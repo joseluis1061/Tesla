17d9ee02d468327e82e8c50e7c5424af13845fdc
José Luis Zúñiga <jose@tangibledesign.co>
2024-04-23 13:20:02 -0500

    Overlay para for mobil

--- src/app/modules/webside/components/menu-mobile/menu-mobile.component.ts
+++ src/app/modules/webside/components/menu-mobile/menu-mobile.component.ts
@@ -2,7 +2,8 @@ import { Component, Inject } from '@angular/core';
 import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
 @Component({
   selector: 'app-menu-mobile',
-  templateUrl: './menu-mobile.component.html'
+  templateUrl: './menu-mobile.component.html',
+  styles: [':host {display: block; position: absolute; top: 0; left:0;}']
 })
 export class MenuMobileComponent {
   constructor(
